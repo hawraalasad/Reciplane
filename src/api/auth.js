57b5c17112ce5bf527a42091bf26ce56d4ae73e8
AbdulazizMurad <abdulaziz.murad2001@gmail.com>
2024-10-07 17:37:34 +0300

    auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,24 +1,8 @@
 import instance from "./index";
-import { setToken, deleteToken } from "./storage";
 
-const login = async (user) => {
-  try {
-    const res = await instance.post("/auth/login", user);
-    setToken(res.data.token);
-    return res.data;
-  } catch (error) {
-    console.log(error);
-  }
+const getAllRecipes = async () => {
+  const response = await instance.get("/api/recipes");
+  return response.data;
 };
 
-const register = async (user) => {
-  const res = await instance.post("/auth/register", user);
-  return res.data;
-};
-
-const logout = async () => {
-  await instance.post("/auth/logout");
-  deleteToken();
-};
-
-export { login, register, logout };
+export { getAllRecipes };
--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,7 @@
 import axios from "axios";
-import { getToken } from "./storage";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:8000",
 });
 
-instance.interceptors.request.use((config) => {
-  const token = getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
 export default instance;
